0ba89d71efbd71516183cc5fe60f3ed91f390164
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2021-11-23 20:53:51 +0000

    Save settings in a map

--- src/main/java/io/github/nickacpt/buildingassister/model/PlayerMirrorSettingsStorage.java
+++ src/main/java/io/github/nickacpt/buildingassister/model/PlayerMirrorSettingsStorage.java
@@ -1,12 +1,17 @@
 package io.github.nickacpt.buildingassister.model;
 
+import java.util.*;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
 public class PlayerMirrorSettingsStorage {
+    private static final Map<UUID, PlayerMirrorSettings> playerMirrorSettings = new HashMap<>();
+
     @Nullable
     public static PlayerMirrorSettings getPlayerMirrorSettings(Player player) {
-        return new PlayerMirrorSettings(new Location(player.getWorld(), 8.5, 4, 8.5), true, false, true, true, false, true);
+        return playerMirrorSettings.computeIfAbsent(player.getUniqueId(),
+                uuid -> new PlayerMirrorSettings(new Location(player.getWorld(), 8.5, 4, 8.5), true, false, true, true,
+                        false, true));
     }
 }
